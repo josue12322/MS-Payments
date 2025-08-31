9356a459d947ddbfb045d67b75e3f130ab5afd2a
Josue Silva Santos Junior <josue.junio@grupomateus.com>
2025-08-31 16:36:41 -0300

    chore: first commit

---
+++ src/main/java/com/payments/hrpay/HrPayApplication.java
@@ -0,0 +1,13 @@
+package com.payments.hrpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/payments/hrpay/HrPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payments.hrpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
